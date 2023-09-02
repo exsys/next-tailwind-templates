610ce83d75350f0abf1c612b83ebbc43e1030ce2
exsyss <exsysb@gmail.com>
2023-09-02 07:26:13 +0200

    update project structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Container2 from "@/components/container/container2";
-import Container1 from "@/components/container/containers1";
-import Hero1 from "@/components/container/hero1";
+import Container2 from "@/components/sections/container2";
+import Container1 from "@/components/sections/containers1";
+import Hero1 from "@/components/sections/hero1";
 import Socials1 from "@/components/design/socials1";
 import Form1 from "@/components/forms/form1";
 import Form2 from "@/components/forms/form2";
@@ -9,7 +9,7 @@ import Footer2 from "@/components/layout/footer2";
 import Header1 from "@/components/layout/header1";
 import Header2 from "@/components/layout/header2";
 import Header3 from "@/components/layout/header3";
-import PriceCard1 from "@/components/sections/priceCard1";
+import PriceCard1 from "@/components/design/priceCard1";
 
 import { priceCards } from "@/assets/priceCardInfos";
 
